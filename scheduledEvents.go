30acbe527449971f324ea4fe8e55792c779391fa
Shawn Smith <ShawnSmith0828@gmail.com>
2022-03-12 10:22:47 -0500

    Finish the Scheduled Events endpoint
    Closes: #2

--- scheduledEvents.go
+++ scheduledEvents.go
@@ -19,7 +19,7 @@ type Event struct {
 	CreatedAt        time.Time          `json:"created_at"`
 	UpdatedAt        time.Time          `json:"updated_at"`
 	EventMemberships []EventMemberships `json:"event_memberships"`
-	EventGuests      []EventGuests      `json:"event_guests"`
+	EventGuests      []Guest            `json:"event_guests"`
 }
 
 type Location struct {
@@ -37,23 +37,232 @@ type EventMemberships struct {
 	User string `json:"user"`
 }
 
-type EventGuests struct {
+// Guest holds a Calendly Guest object
+type Guest struct {
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-// EventResponse is the Calendly Event response from their API
-type EventResponse struct {
+// Invitee holds a Calendly Invitee object
+type Invitee struct {
+	CancelURL           string                      `json:"cancel_url"`
+	CreatedAt           time.Time                   `json:"created_at"`
+	Email               string                      `json:"email"`
+	Event               string                      `json:"event"`
+	FirstName           string                      `json:"first_name"`
+	LastName            string                      `json:"last_name"`
+	Name                string                      `json:"name"`
+	NewInvitee          interface{}                 `json:"new_invitee"`
+	OldInvitee          interface{}                 `json:"old_invitee"`
+	QuestionsAndAnswers []CustomQuestionsAndAnswers `json:"questions_and_answers"`
+	RescheduleURL       string                      `json:"reschedule_url"`
+	Rescheduled         bool                        `json:"rescheduled"`
+	Status              string                      `json:"status"`
+	TextReminderNumber  interface{}                 `json:"text_reminder_number"`
+	Timezone            string                      `json:"timezone"`
+	Tracking            Tracking                    `json:"tracking"`
+	UpdatedAt           time.Time                   `json:"updated_at"`
+	URI                 string                      `json:"uri"`
+	Canceled            bool                        `json:"canceled"`
+	Cancellation        Cancellation                `json:"cancellation"`
+	Payment             Payment                     `json:"payment"`
+	NoShow              string                      `json:"no_show"`
+}
+
+type Cancellation struct {
+	CanceledBy string `json:"canceled_by"`
+	Reason     string `json:"reason"`
+}
+
+type Payment struct {
+	ExternalID string  `json:"external_id"`
+	Provider   string  `json:"provider"`
+	Amount     float64 `json:"amount"`
+	Currency   string  `json:"currency"`
+	Terms      string  `json:"terms"`
+	Successful bool    `json:"successful"`
+}
+
+// getEventResponse is the Calendly get event response from their API
+type getEventResponse struct {
 	Resource Event `json:"resource"`
+}
+
+// getEventInviteeResponse is the Calendly get event invitee response
+type getEventInviteeResponse struct {
+	Resource Invitee `json:"resource"`
+}
+
+// listEventsResponse is the Calendly list events response from their API
+type listEventsResponse struct {
+	Collection []Event `json:"collection"`
+}
+
+// listEventInviteesResponse is the Calendly list event invitees response from their API
+type listEventInviteesResponse struct {
+	Collection []Invitee `json:"collection"`
+}
+
+type ListEventInviteesInput struct {
+	ID        string
+	Count     int
+	Email     string
+	PageToken string
+	Sort      string
+	Status    string
+}
+
+type ListEventsInput struct {
+	Count        int
+	InviteeEmail string
+	MaxStartTime string
+	MinStartTime string
+	Organization string
+	PageToken    string
+	Sort         string
+	Status       string
+	User         string
+}
+
+// ListEventInvitees lists all the invitees for a specific event
+func (cw *CalendlyWrapper) ListEventInvitees(input *ListEventInviteesInput) ([]Invitee, error) {
+	var invList []Invitee
+	var invListResp listEventInviteesResponse
+
+	if input.ID == "" {
+		return invList, fmt.Errorf("you must provide an ID")
+	}
+
+	url := fmt.Sprintf("%s%s%s%s", cw.baseApiUrl, "scheduled_events/", input.ID, "/invitees")
+
+	if input.Count >= 1 && input.Count <= 100 {
+		url += fmt.Sprintf("?count=%d", input.Count)
+	} else {
+		// Because the first param uses ? instead of & we always use count
+		// Easier than trying to dynamically add ? to the first param
+		url += "?count=20"
+	}
+
+	if input.Email != "" {
+		url += fmt.Sprintf("&email=%s", input.Email)
+	}
+
+	if input.PageToken != "" {
+		url += fmt.Sprintf("&page_token=%s", input.PageToken)
+	}
+
+	if input.Sort != "" {
+		url += fmt.Sprintf("&sort=%s", input.Sort)
+	}
+
+	if input.Status != "" {
+		url += fmt.Sprintf("&status=%s", input.Status)
+	}
+
+	resp, err := cw.sendGetReq(url)
+	if err != nil {
+		return invList, err
+	}
+
+	err = json.Unmarshal(resp, &invListResp)
+	if err != nil {
+		return invList, err
+	}
+
+	invList = invListResp.Collection
+
+	return invList, nil
+}
+
+// ListEvents returns an event list from the Calendly API
+func (cw *CalendlyWrapper) ListEvents(input *ListEventsInput) ([]Event, error) {
+	var events []Event
+	var eventsResponse listEventsResponse
+
+	url := fmt.Sprintf("%s/scheduled_events", cw.baseApiUrl)
+
+	if input.Count >= 1 && input.Count <= 100 {
+		url += fmt.Sprintf("?count=%d", input.Count)
+	} else {
+		// Because the first param uses ? instead of & we always use count
+		// Easier than trying to dynamically add ? to the first param
+		url += "?count=20"
+	}
+
+	if input.MaxStartTime != "" {
+		url += fmt.Sprintf("&max_start_time=%s", input.MaxStartTime)
+	}
+
+	if input.MinStartTime != "" {
+		url += fmt.Sprintf("&min_start_time=%s", input.MinStartTime)
+	}
+
+	if input.Organization != "" {
+		url += fmt.Sprintf("&organization=%s", input.Organization)
+	}
+
+	if input.InviteeEmail != "" {
+		url += fmt.Sprintf("&email=%s", input.InviteeEmail)
+	}
+
+	if input.PageToken != "" {
+		url += fmt.Sprintf("&page_token=%s", input.PageToken)
+	}
+
+	if input.Sort != "" {
+		url += fmt.Sprintf("&sort=%s", input.Sort)
+	}
+
+	if input.Status != "" {
+		url += fmt.Sprintf("&status=%s", input.Status)
+	}
+
+	if input.User != "" {
+		url += fmt.Sprintf("&user=%s", input.User)
+	}
+
+	resp, err := cw.sendGetReq(url)
+	if err != nil {
+		return events, err
+	}
+
+	err = json.Unmarshal(resp, &eventsResponse)
+	if err != nil {
+		return events, err
+	}
+
+	events = eventsResponse.Collection
+
+	return events, nil
+}
+
+// GetEventInvitee returns an invitee for a specific event
+func (cw *CalendlyWrapper) GetEventInvitee(eventid, inviteeid string) (Invitee, error) {
+	var inv Invitee
+	var invResp getEventInviteeResponse
+
+	resp, err := cw.sendGetReq(fmt.Sprintf("%sscheduled_events/%s/invitees/%s", cw.baseApiUrl, eventid, inviteeid))
+	if err != nil {
+		return inv, err
+	}
+
+	err = json.Unmarshal(resp, &invResp)
+	if err != nil {
+		return inv, err
+	}
+
+	inv = invResp.Resource
+
+	return inv, nil
 }
 
 // GetEvent gets the specified Calendly event
 func (cw *CalendlyWrapper) GetEvent(id string) (Event, error) {
 	var event Event
-	var eventResponse EventResponse
+	var eventResponse getEventResponse
 
-	resp, err := cw.sendGetReq(fmt.Sprintf("%s%s%s", cw.baseApiUrl, "scheduled_events/", id))
+	resp, err := cw.sendGetReq(fmt.Sprintf("%sscheduled_events/%s", cw.baseApiUrl, id))
 	if err != nil {
 		return event, err
 	}
--- scheduledEvents_test.go
+++ scheduledEvents_test.go
@@ -6,6 +6,77 @@ import (
 	"github.com/helixspiral/calendly"
 )
 
+func TestListEventInvitees(t *testing.T) {
+	cw := calendly.New(&calendly.CalendlyWrapperInput{
+		BaseApiUrl: "https://stoplight.io/mocks/calendly/api-docs/395/",
+		CustomHeaders: map[string]string{
+			"Prefer": "code=200",
+		},
+	})
+
+	invList, err := cw.ListEventInvitees(&calendly.ListEventInviteesInput{
+		ID:        "AAAAAAAAAAAAAAAA",
+		Count:     1,
+		Email:     "test@example.com",
+		PageToken: "test",
+		Sort:      "created_at:asc",
+		Status:    "active",
+	})
+	if err != nil {
+		t.Fatal("Failed to get invitee list:", err)
+	}
+
+	if invList[0].Name != "John Doe" {
+		t.Fatalf("Expected 'John Doe', received '%s'.", invList[0].Name)
+	}
+}
+
+func TestListEvents(t *testing.T) {
+	cw := calendly.New(&calendly.CalendlyWrapperInput{
+		BaseApiUrl: "https://stoplight.io/mocks/calendly/api-docs/395/",
+		CustomHeaders: map[string]string{
+			"Prefer": "code=200",
+		},
+	})
+
+	eventList, err := cw.ListEvents(&calendly.ListEventsInput{
+		Count:        1,
+		InviteeEmail: "user@example.com",
+		MaxStartTime: "2019-08-24T14:15:22Z",
+		MinStartTime: "2019-08-24T14:15:22Z",
+		Organization: "https://api.calendly.com/organization/AAAAAAAAAAAAAAAA",
+		PageToken:    "test",
+		Sort:         "created_at:asc",
+		Status:       "active",
+		User:         "https://api.calendly.com/users/AAAAAAAAAAAAAAAA",
+	})
+	if err != nil {
+		t.Fatal("Failed to get event list:", err)
+	}
+
+	if eventList[0].Name != "15 Minute Meeting" {
+		t.Fatalf("Expected '15 Minute Meeting', received '%s'.", eventList[0].Name)
+	}
+}
+
+func TestGetEventInvitee(t *testing.T) {
+	cw := calendly.New(&calendly.CalendlyWrapperInput{
+		BaseApiUrl: "https://stoplight.io/mocks/calendly/api-docs/395/",
+		CustomHeaders: map[string]string{
+			"Prefer": "code=200",
+		},
+	})
+
+	inv, err := cw.GetEventInvitee("test-event", "test-invitee")
+	if err != nil {
+		t.Fatal("Failed to get event:", err)
+	}
+
+	if inv.Name != "John Doe" {
+		t.Fatalf("Expected 'John Doe', received '%s'", inv.Name)
+	}
+}
+
 func TestGetEvent(t *testing.T) {
 	cw := calendly.New(&calendly.CalendlyWrapperInput{
 		BaseApiUrl: "https://stoplight.io/mocks/calendly/api-docs/395/",
